05572f2a9435f7cf41d2e032182ecae86342888d
Sytus_Ramlethal <zcy1810778545@163.com>
2024-09-10 15:54:05 +0800

    feat: home页面初始化

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,11 +1,62 @@
 import { memo } from 'react'
-
+import React from 'react';
+import { Layout, Menu, theme } from 'antd';
+import { CloudOutlined } from '@ant-design/icons';
+import style from './index.module.css'
+const { Content, Sider } = Layout;
 const Home = () => {
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
     return (
-        <>
-            This is Home.
-        </>
-    )
+        <Layout style={{
+            height: '100vh'
+        }}>
+            <Sider
+                width={200}
+                breakpoint="lg"
+                collapsedWidth="0"
+                onBreakpoint={(broken) => {
+                    console.log(broken);
+                }}
+                onCollapse={(collapsed, type) => {
+                    console.log(collapsed, type);
+                }}
+                style={{
+                    background: colorBgContainer,
+                }}
+            >
+                <div className={style.logo}>论文管理系统</div>
+                <Menu
+                    mode="inline"
+                    items={[
+                        {
+                            key: '/home',
+                            icon: <CloudOutlined />,
+                            label: '云盘',
+                        }
+                    ]}
+                />
+            </Sider>
+            <Layout
+                style={{
+                    padding: '0 24px 24px',
+                }}
+            >
+                <Content
+                    style={{
+                        padding: 24,
+                        margin: 0,
+                        minHeight: 280,
+                        background: colorBgContainer,
+                        borderRadius: borderRadiusLG,
+                    }}
+                >
+                    Content
+                </Content>
+            </Layout>
+        </Layout>
+    );
 }
 
 export const MemoHome = memo(Home)
