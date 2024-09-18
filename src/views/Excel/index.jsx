2bb4dc428573c77254ccae802579987f1f66d6cb
Sytus_Ramlethal <zcy1810778545@163.com>
2024-09-18 18:52:24 +0800

    feat: 解决toolbar频闪问题

--- src/views/Excel/index.jsx
+++ src/views/Excel/index.jsx
@@ -2,15 +2,18 @@ import { memo, useRef, useState, useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { Button, Drawer, Form, Input, Space, Spin } from 'antd';
 import { MemoSheet } from '@/components/UniverSheet';
-import { DEFAULT_WORKBOOK_DATA } from '@/assets/default-data'
+import { useMessage } from '@/hooks/useMessage';
 import { getExcelDetail } from '@/apis/excel';
+import style from './index.module.css'
 
 const Excel = () => {
     const param = useParams()
     const univerRef = useRef()
     const excelName = useRef('')
+    const { success, error, contextHolder } = useMessage()
     const [data, setData] = useState(false);
     const [title, setTitle] = useState('')
+    const [loading, setLoading] = useState(true)
     const navigate = useNavigate()
     const getDetail = async (id = param.id) => {
         const res = await getExcelDetail(id)
@@ -18,13 +21,33 @@ const Excel = () => {
         setData(JSON.parse(url))
         excelName.current = title
         setTitle(title)
+        setLoading(false)
     }
     useEffect(() => {
-        getDetail(param.id)
+        try {
+            getDetail(param.id)
+        } catch (e) {
+
+        }
     }, [])
+    useEffect(() => {
+        if (!loading) {
+            setTimeout(() => {
+                const toolbar = document.querySelector('.univer-toolbar');
+                if (toolbar) {
+                    toolbar.style.opacity = '1';
+                }
+            }, 500);
+        }
+    }, [loading]);
     return (
         <>
-            <MemoSheet style={{ flex: 1 }} ref={univerRef} data={data} />
+            {contextHolder}
+            {
+                loading
+                    ? <Spin />
+                    : <MemoSheet style={{ flex: 1 }} ref={univerRef} data={data} />
+            }
         </>
     )
 }
