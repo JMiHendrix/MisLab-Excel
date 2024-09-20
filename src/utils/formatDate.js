79aef5ded6d7c68b0eef7824c08e2a93ef5567f6
Sytus_Ramlethal <zcy1810778545@163.com>
2024-09-20 16:07:41 +0800

    feat: 格式化时间补0

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -3,9 +3,9 @@ const formatDate = (time) => {
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
     const day = date.getDate();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
 
     return `${year}年${month}月${day}日 ${hours}:${minutes}:${seconds}`;
 }
